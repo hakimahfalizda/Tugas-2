e28a79244fb351ec08deda443757844fcd1ea28c
falizda <anggitwidiantoro187@gmail.com>
2019-03-21 17:49:35 +0700

    fisrt commit #3

---
+++ app/src/main/java/com/faliz/cobarabu/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.faliz.cobarabu
+
+import android.os.Bundle
+import android.support.v4.view.ViewPager
+import android.support.v7.app.AppCompatActivity
+import com.ogaclejapan.smarttablayout.SmartTabLayout
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItemAdapter
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItems
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val adapter = FragmentPagerItemAdapter(
+            supportFragmentManager, FragmentPagerItems.with(this)
+                .add("Fashion", firstfragment::class.java)
+                .add("Anak TI 2016", secondFragment::class.java)
+                .add("Asal Mula Daerah", threeFragment::class.java)
+                .add("Profile", fourFragment::class.java)
+                .create()
+        )
+
+        val viewPager = findViewById(R.id.viewpager) as ViewPager
+        viewPager.adapter = adapter
+
+        val viewPagerTab = findViewById(R.id.viewpagertab) as SmartTabLayout
+        viewPagerTab.setViewPager(viewPager)
+    }
+}
---
+++ app/src/main/java/com/faliz/cobarabu/three.kt
@@ -0,0 +1,4 @@
+package com.faliz.cobarabu
+
+class three {
+}
